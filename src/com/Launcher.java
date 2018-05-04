59bf1146ef2dc3c2e4a1ce62a924cbe60ee6596d
Ckozan <Cameron.kozan@gmail.com>
2018-05-04 16:20:26 -0400

    Register function checks database
    Updated the register function to check to see if the user whom is being
    registered is already inside of the database

--- src/com/Launcher.java
+++ src/com/Launcher.java
@@ -1,17 +1,14 @@
 package com;
 
-import com.awt.LoginFrame;
 import com.awt.WelcomeScreen;
-import com.misc.Misc;
 import com.net.MySQL;
-import com.table.Table;
 
 public class Launcher {
 	
 	public static void main(String[] args) {
 		MySQL sql = new MySQL();
-		Table table = new Table();
-		table.createTable();
+		WelcomeScreen welcome = new WelcomeScreen("Welcome!", sql);
+		welcome.createFrame();
 	}
 	
 }
--- src/com/net/MySQL.java
+++ src/com/net/MySQL.java
@@ -2,11 +2,11 @@ package com.net;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.misc.Misc;
-
 
 public class MySQL {
 
@@ -28,20 +28,28 @@ public class MySQL {
 	}
 
 	public boolean register(String username, String password, long key) {
-		if (insertUser(username, Misc.hashString(password, key), key)) {
-			System.out.println(true);
-			return true;
-		} else {
-			System.out.println(false);
-			return false;
+		ResultSet rs = selectAll("users", "name", username);
+		try {
+			while(rs.next()) {
+				if(rs.getString("name") != null) {
+					return false;
+				}
+				break;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		insertUser(username, Misc.hashString(password, key), key);
+		return true;
+
 	}
 
 	public boolean insertUser(String username, String password, long key) {
-		System.out.println("Username: "+username);
-		System.out.println("Password: "+password);
-		System.out.println("Key: "+key);
-		String sql = "INSERT INTO users (name, password, passKey) VALUES ('" + username + "', '"+password+"', '"+key+"')";
+		System.out.println("Username: " + username);
+		System.out.println("Password: " + password);
+		System.out.println("Key: " + key);
+		String sql = "INSERT INTO users (name, password, passKey) VALUES ('" + username + "', '" + password + "', '"
+				+ key + "')";
 		try {
 			statement.execute(sql);
 			return true;
@@ -50,7 +58,17 @@ public class MySQL {
 			return false;
 		}
 
+	}
 
+	public ResultSet selectAll(String table, String element, String index) {
+		String sql = "SELECT * FROM " + table + " WHERE  " + element + " = '" + index + "'";
+		try {
+			statement.execute(sql);
+			return statement.getResultSet();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
